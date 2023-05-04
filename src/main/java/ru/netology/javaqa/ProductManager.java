bc704f997d6cfffe616c3f105ad695e956457173
Dolgov David <dolgov.david.1993@gmail.com>
2023-05-04 16:24:09 +0300

    rich model

--- src/main/java/ru/netology/javaqa/ProductManager.java
+++ src/main/java/ru/netology/javaqa/ProductManager.java
@@ -11,15 +11,12 @@ public class ProductManager {
         repository.save(product);
     }
 
-    // метод определения соответствия товара product запросу search
-    public boolean matches(Product product, String search) {
-        return product.getName().contains(search);     // Возвращает true или false
-    }
+
 
     public Product[] searchBy(String text) {
         Product[] result = new Product[0];       // тут будем хранить подошедшие запросу продукты
         for (Product product : repository.findAll()) {
-            if (matches(product, text)) {
+            if (product.matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
                 for (int i = 0; i < result.length; i++) {
                     tmp[i] = result[i];
--- src/main/java/ru/netology/javaqa/Smartphone.java
+++ src/main/java/ru/netology/javaqa/Smartphone.java
@@ -8,4 +8,14 @@ public class Smartphone extends Product {
         this.manufacturer = manufacturer;
     }
 
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (super.matches(product, search) == false) {
+            return getManufacturer().contains(search);
+        }
+        return true;
+    }
 }
